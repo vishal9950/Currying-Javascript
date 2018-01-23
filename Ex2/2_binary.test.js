fa46c36789bddd98f5bd65837e692b0dda4abcfe
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-23 23:44:06 +0530

    Ex2: #TC4: add two negative numbers

--- Ex2/2_binary.test.js
+++ Ex2/2_binary.test.js
@@ -14,4 +14,8 @@ describe('Should add: ', () => {
   test('Two positive numbers 2 and 3: ', () => {
     expect(sum(2, 3)).toBe(5);
   });
+
+  test('Two negative numbers -1 and -5: ', () => {
+    expect(sum(-1, -5)).toBe(-6);
+  });
 });
