1013db0aef0c5d27ed2571371a01597a736323da
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-24 01:32:18 +0530

    Ex4: #TC3: 1 arg

--- Ex4/4_longDelay.js
+++ Ex4/4_longDelay.js
@@ -1,9 +1,15 @@
+total = 0;
+
 const longDelay = (args) => {
-  if ((args === null) || (args === undefined)) {
-    console.log(`Err1: ${args} arg supplied`);
-    return args;
+  // if ((args === null) || (args === undefined)) {
+  //   console.log(`Err1: ${args} arg supplied`);
+  //   return args;
+  // }
+  if (typeof args !== 'number') {
+    return total;
   }
-  return true;
+  total += args;
+  return longDelay;
 };
 
 module.exports = longDelay;
--- Ex4/4_longDelay.test.js
+++ Ex4/4_longDelay.test.js
@@ -2,10 +2,16 @@ const longDelay = require('./4_longDelay');
 
 describe('Argument Validation: ', () => {
   test('Should not work for null arg: ', () => {
-    expect(longDelay(null)).toBe(null);
+    expect(longDelay(null)).toBe(0);
   });
 
   test('Should not work for undefined arg: ', () => {
-    expect(longDelay(undefined)).toBe(undefined);
+    expect(longDelay(undefined)).toBe(0);
+  });
+});
+
+describe('Working of function longDelay: ', () => {
+  test('Should work for one argument: ', () => {
+    expect(typeof longDelay(1)).toBe('function');
   });
 });
