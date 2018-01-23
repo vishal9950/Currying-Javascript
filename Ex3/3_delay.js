dc035568df942a47809d36088f510673d315f398
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-24 00:34:31 +0530

    Ex3: #TC2: undefined args

--- Ex3/3_delay.js
+++ Ex3/3_delay.js
@@ -5,11 +5,23 @@ const verifyIfNull = (arg) => {
   return true;
 };
 
+const verifyIfUndef = (arg) => {
+  if (arg === undefined) {
+    return false;
+  }
+  return true;
+};
+
 const someFunc = (first) => {
   const nullCheck = verifyIfNull(first);
   if (!nullCheck) {
     console.log('Err1: null argument supplied');
     return null;
+  }
+  const undefCheck = verifyIfUndef(first);
+  if (!undefCheck) {
+    console.log('Err2: undefined arg supplied');
+    return undefined;
   }
   return true;
 };
