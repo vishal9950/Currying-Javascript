a7a4b0c4c4c28ead1ce82f7aa4647f19326be3cd
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-24 10:48:01 +0530

    Ex2: #TC7: Use a wrapper

--- Ex2/2_binary.js
+++ Ex2/2_binary.js
@@ -5,20 +5,28 @@ function verifyType(arg) {
   return false;
 }
 
-function sum(first, second) {
-  if ((first === null) || (first === undefined)) {
-    console.log(`Err1: ${first} argument supplied`);
-    return first;
+
+function validate(sum) {
+  function wrapper(first, second) {
+    if ((first === null) || (first === undefined)) {
+      console.log(`Err1: ${first} argument supplied`);
+      return first;
+    }
+    if ((second === null) || (second === undefined)) {
+      console.log(`Err1: ${second} argument supplied`);
+      return second;
+    }
+    const typeCheck = verifyType(first) && verifyType(second);
+    if (!typeCheck) {
+      return false;
+    }
+    return sum(first, second);
   }
-  if ((second === null) || (second === undefined)) {
-    console.log(`Err1: ${second} argument supplied`);
-    return second;
-  }
-  const typeCheck = verifyType(first) && verifyType(second);
-  if (!typeCheck) {
-    return false;
-  }
-  return first + second;
+  return wrapper;
 }
 
-module.exports = sum;
+const sum = (first, second) => first + second;
+
+const sum1 = validate(sum);
+
+module.exports = sum1;
